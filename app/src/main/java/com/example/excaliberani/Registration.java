aac9c17fa8e3df631fb77a768be54c69f5abcb88
Shivanshi <shivanshi308@gmail.com>
2019-09-23 19:34:31 +0530

    registration backend, remember user login status.

--- app/src/main/java/com/example/excaliberani/Registration.java
+++ app/src/main/java/com/example/excaliberani/Registration.java
@@ -1,14 +1,116 @@
 package com.example.excaliberani;
 
+//registration
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Registration extends AppCompatActivity {
+
+
+    private ProgressDialog progressDialog;
+    Button proceedButton;
+    EditText username,emailid,phone,passwd; //for fetching details from editText(registration form)
+    String name,email,phonenumber,password,body; //where actual details of user are stored
+    private DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        Intent intent=getIntent();
+        username = (EditText) findViewById(R.id.rname);
+        emailid = (EditText) findViewById(R.id.remail);
+        phone = (EditText) findViewById(R.id.rmob);
+        passwd = (EditText) findViewById(R.id.rpass);
+        proceedButton = (Button) findViewById(R.id.rbutton);
+        progressDialog = new ProgressDialog(this);
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+
+        final Userdetails userdetails= new Userdetails();
+        proceedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                validateDataFromEditText();
+                progressDialog.setMessage("Registering...");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progressDialog.setCanceledOnTouchOutside(false);
+                Email orig_mail= new Email(email);
+                final String key_email=orig_mail.convert_mail(email);
+
+                userdetails.setemail(email);
+                userdetails.setname(name);
+                userdetails.setphonenumber(phonenumber);
+                userdetails.setpassword(password);
+                databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(!dataSnapshot.child("users").child(key_email).exists()){
+                            Task<Void> Tb= databaseReference.child("users").child(key_email).setValue(userdetails).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        progressDialog.dismiss();
+                                        Toast.makeText(Registration.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else{
+                                        progressDialog.dismiss();
+                                        Toast.makeText(Registration.this, "Error! Try Again", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+
+    public void validateDataFromEditText(){
+
+        name=username.getText().toString().trim();
+        email=emailid.getText().toString().trim();
+        phonenumber=phone.getText().toString().trim();
+        password=passwd.getText().toString().trim();
+        if(name.isEmpty() || email.isEmpty() || phonenumber.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Fill all fields", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                Toast.makeText(this, "Enter valid email address", Toast.LENGTH_SHORT).show();
+            else if (phonenumber.length() != 10) {
+                Toast.makeText(this, "Enter valid phone number", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                return;
+            }
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/excaliberani/RememberUser.java
@@ -0,0 +1,61 @@
+package com.example.excaliberani;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+
+//remembers previously logged in user
+
+public class RememberUser {
+    SharedPreferences preferences;
+    Context context;
+    SharedPreferences.Editor editor;
+    private static final String PREF_NAME="UserDetails";
+    private static final String PREF_USER_NAME="username";
+    int PRIVATE_MODE=0;
+    private static final String IS_USER_LOGIN="IsUserLoggedIn";
+    public static final String EMAIL="email";
+    public static final String PASSWORD="password";
+
+    public RememberUser(Context context){
+        this.context=context;
+        preferences=context.getSharedPreferences(PREF_USER_NAME,PRIVATE_MODE);
+        editor=preferences.edit();
+    }
+
+    public void createLoginSession(String email, String password){
+
+        editor.putBoolean(IS_USER_LOGIN, true);
+        editor.putString(PASSWORD, password);
+        editor.putString(EMAIL, email);
+        editor.commit();
+    }
+
+    public boolean isLoggedIn(){
+        return preferences.getBoolean(IS_USER_LOGIN,false);
+    }
+
+    public void checkLogin(){
+        if(!this.isLoggedIn()){
+            Intent intent=new Intent(context,Login_activity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+            //        return true;
+        }
+        //   return false;
+    }
+
+    public HashMap<String, String> getUserDetails(){
+
+        HashMap<String, String> user = new HashMap<>();
+        user.put(EMAIL,preferences.getString(EMAIL, null));
+        user.put(PASSWORD,preferences.getString(PASSWORD, null));
+
+        return user;
+    }
+
+
+}
