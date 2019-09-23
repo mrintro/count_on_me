485b6aab0906b06e85f24fd35b24c008d1eddc34
Shashank2409 <shashankgupta588@gmail.com>
2019-09-23 18:57:01 +0530

    LOGIN ACTIVITY SEND CANCEL ACCEPT AND DECLINE FRIEND REQ ACCEPT REQ FROM NEWS FEEDS

--- app/src/main/java/com/example/excaliberani/Login_activity.java
+++ app/src/main/java/com/example/excaliberani/Login_activity.java
@@ -1,18 +1,117 @@
+//LOGIN PAGE USING AUTH FIREBASE BUT NOT USING SHARED PREFERENCES
+
 package com.example.excaliberani;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login_activity extends AppCompatActivity {
     float x1,x2,y1,y2;
+    EditText emailId, password;
+    Button btnSignIn,aniketWalaKaam;
+    TextView tvSignUp;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.lemail);
+        password = findViewById(R.id.lpass);
+        btnSignIn = findViewById(R.id.lbutton);
+        //tvSignUp = findViewById(R.id.textView);
+        //aniketWalaKaam = findViewById(R.id.excal_final_accpet_request);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                // Toast.makeText(LoginActivity.this,mFirebaseUser.toString(),Toast.LENGTH_LONG).show();
+                if( mFirebaseUser != null ){
+                    Toast.makeText(Login_activity.this,"You are logged in",Toast.LENGTH_SHORT).show();
+                    //Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                    //startActivity(i);
+                }
+                else{
+                    //Toast.makeText(LoginActivity.this,"Please Login",Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailId.getText().toString();
+                String pwd = password.getText().toString();
+                if(email.isEmpty()){
+                    emailId.setError("Please enter email id");
+                    emailId.requestFocus();
+                }
+                else  if(pwd.isEmpty()){
+                    password.setError("Please enter your password");
+                    password.requestFocus();
+                }
+                else  if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(Login_activity.this,"Fields Are Empty!",Toast.LENGTH_SHORT).show();
+                }
+                else  if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(Login_activity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(Login_activity.this,"Login Error, Please Login Again",Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Intent intToHome = new Intent(Login_activity.this,MainActivity.class);
+                                startActivity(intToHome);
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(Login_activity.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+//        tvSignUp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intSignUp = new Intent(Login_activity.this, MainActivity.class);
+//                startActivity(intSignUp);
+//                finish();
+//            }
+//        });
+//
+//        aniketWalaKaam.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i=new Intent(Login_activity.this,accept_req_page.class);
+//                startActivity(i);
+//            }
+//        });
+
     }
 
     public boolean onTouchEvent(MotionEvent touchEvent){
@@ -31,5 +130,10 @@ public class Login_activity extends AppCompatActivity {
                 break;
         }
         return false;
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 }
---
+++ app/src/main/java/com/example/excaliberani/friendRequest.java
@@ -0,0 +1,136 @@
+//TO ACCEPT FRIEND REQUEST AND TO CANCEL FRIEND REQUEST PRESENT IN "MY FRIEND REQ"..
+
+package com.example.excaliberani;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class friendRequest extends AppCompatActivity {
+    EditText receiver,sender;
+    Button accept,decline;
+    DatabaseReference mData,mFriendData;
+    String mCurrentState;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_request);
+
+        sender=findViewById(R.id.excal_sender);
+        receiver=findViewById(R.id.excal_receiver);
+        accept=findViewById(R.id.excal_accept);
+        decline=findViewById(R.id.excal_decline);
+        mData= FirebaseDatabase.getInstance().getReference();
+        mFriendData= FirebaseDatabase.getInstance().getReference();
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(friendRequest.this,"CheckPoint1",Toast.LENGTH_LONG).show();
+                final String senderName=sender.getText().toString().trim();
+                final String receiverName=receiver.getText().toString().trim();
+                mData.child("friend_req").child(receiverName).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Toast.makeText(friendRequest.this,"CheckPoint2",Toast.LENGTH_LONG).show();
+                        if(dataSnapshot.hasChild(senderName)){
+                            String req_type=dataSnapshot.child(senderName).child("request_type").getValue().toString().trim();
+
+                            Toast.makeText(friendRequest.this,"CheckPoint3",Toast.LENGTH_LONG).show();
+                            if(req_type.equals("received")){
+                                mFriendData.child("friendList").child(senderName).child(receiverName).setValue("friends");
+                                mFriendData.child("friendList").child(receiverName).child(senderName).setValue("friends");
+                                decline.setEnabled(false);
+                                mData.child("friend_req").child(senderName).child(receiverName).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        mData.child("friend_req").child(receiverName).child(senderName).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Toast.makeText(friendRequest.this,"Request removed successfully",Toast.LENGTH_LONG);
+                                                //sendReq.setEnabled(true);
+                                                mData.child("friend_req").child(senderName).child(receiverName).removeValue();
+                                                mData.child("friend_req").child(receiverName).child(senderName).removeValue();
+                                                //mCurrentStatus="not_friends";
+                                                //sendReq.setText("Send request");
+                                            }
+                                        });
+                                    }
+                                });
+                                Toast.makeText(friendRequest.this,"CheckPoint4",Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                Toast.makeText(friendRequest.this,"Did not find any req",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(friendRequest.this,"Some Error occured",Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+        decline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(friendRequest.this,"CheckPoint1",Toast.LENGTH_LONG).show();
+                final String senderName=sender.getText().toString().trim();
+                final String receiverName=receiver.getText().toString().trim();
+                mData.child("friend_req").child(receiverName).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Toast.makeText(friendRequest.this,"CheckPoint2",Toast.LENGTH_LONG).show();
+                        if(dataSnapshot.hasChild(senderName)){
+                            String req_type=dataSnapshot.child(senderName).child("request_type").getValue().toString().trim();
+
+                            Toast.makeText(friendRequest.this,"CheckPoint3",Toast.LENGTH_LONG).show();
+                            if(req_type.equals("received")){
+                                mData.child("friend_req").child(senderName).child(receiverName).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        mData.child("friend_req").child(receiverName).child(senderName).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Toast.makeText(friendRequest.this,"Request removed successfully",Toast.LENGTH_LONG);
+                                                //sendReq.setEnabled(true);
+                                                mData.child("friend_req").child(senderName).child(receiverName).removeValue();
+                                                mData.child("friend_req").child(receiverName).child(senderName).removeValue();
+                                                //mCurrentStatus="not_friends";
+                                                //sendReq.setText("Send request");
+                                            }
+                                        });
+                                    }
+                                });
+                                accept.setEnabled(false);
+                                Toast.makeText(friendRequest.this,"CheckPoint4",Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                Toast.makeText(friendRequest.this,"Did not find any req",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(friendRequest.this,"Some Error occured",Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+    }
+}
